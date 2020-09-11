d004bf5657ef829967212a0f0a343ca901671714
emermacko <mr.macko.yt@gmail.com>
2020-09-11 21:58:57 +0200

    :sparkles: Added loading bar and changed clear function

--- get_direct_urls.py
+++ get_direct_urls.py
@@ -1,17 +1,16 @@
 from selenium import webdriver
+from tqdm import tqdm
 
-import platform
 import os
 import time
 import argparse
 import pyperclip
+import sys
 
 links = []
 urls = []
 
 def get_urls():
-    os.system(clear)
-
     imgs = driver.find_elements_by_css_selector('.img')
     print(f'[OK] Found {len(imgs)} photos')
 
@@ -20,10 +19,9 @@ def get_urls():
 
     time.sleep(1)
     index = 1
-    for url in urls:
+    for url in tqdm(urls, desc="[OK] Downloading: ", unit="file", ncols=70, bar_format="{desc}{n_fmt}/{total_fmt} {bar} {percentage:3.0f}%"):
         driver.get(url)
         links.append(driver.find_element_by_css_selector('#main-image').get_attribute('src'))
-        print(f'[OK] Getting links [{index}/{len(urls)}]\r', end="")
         index += 1
 
     clipboard = ''
@@ -45,7 +43,8 @@ def check_link(url):
 ####################
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-l', '--link', metavar='[link]', nargs=1, help='Directly link the gallery')
+parser.add_argument('-u', '--url', metavar='url', nargs=1, help='Directly link to the gallery')
+parser.add_argument('-i', '--id', metavar='id', nargs=1, help='Gallery id - last part of the url')
 args = parser.parse_args()
 
 options = webdriver.ChromeOptions()
@@ -53,22 +52,16 @@ options.add_argument('--headless')
 options.add_argument('--log-level=3')
 driver = webdriver.Chrome(options=options)
 
-OS = platform.system()
-if OS == 'Windows':
-    clear = 'cls'
-elif OS == 'Linux' or OS == 'Darwin':
-    clear = 'clear'
-else: clear = ''
+clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+url = args.url[0] if args.url else ''
 
-url = ''
-if args.link:
-    url = args.link[0]
-
-os.system(clear)
+clear()
 while not check_link(url):
     url = input('[>] Link to gallery: ')
 
+clear()
 try:
     get_urls()
 except Exception as e:
     print('[ERROR] ', e)
+
